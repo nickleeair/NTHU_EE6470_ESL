33523a8f9dc300fc2cbbaa7bf2b5eadfd67ec5be
nickleeair <102891463+nickleeair@users.noreply.github.com>
2023-04-11 23:21:33 +0800

    Add files via upload

---
+++ HW2/Filters_QK/Filters.h
@@ -0,0 +1,41 @@
+#ifndef FILTERS_H_
+#define FILTERS_H_
+#include <systemc>
+using namespace sc_core;
+
+#include "tlm"
+#include "tlm_utils/simple_target_socket.h"
+
+#include "filter_def.h"
+
+class Filters : public sc_module
+{
+public:
+  tlm_utils::simple_target_socket<Filters> t_skt;
+
+  sc_fifo<unsigned char> i_r;
+  sc_fifo<unsigned char> i_g;
+  sc_fifo<unsigned char> i_b;
+  sc_fifo<double> or_result;
+  sc_fifo<double> og_result;
+  sc_fifo<double> ob_result;
+
+  SC_HAS_PROCESS(Filters);
+  Filters(sc_module_name n);
+  ~Filters() = default;
+
+private:
+  void do_filter();
+
+
+  double r_val;
+  double g_val;
+  double b_val;
+
+  unsigned int base_offset;
+  void blocking_transport(tlm::tlm_generic_payload &payload,
+                          sc_core::sc_time &delay);
+};
+
+#endif
+
---
+++ HW2/Filters_QK/build/CMakeFiles/filters.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/ee6470/HW2/Filters_QK/Filters.cpp" "/home/user/ee6470/HW2/Filters_QK/build/CMakeFiles/filters.dir/Filters.cpp.o"
+  "/home/user/ee6470/HW2/Filters_QK/Initiator.cpp" "/home/user/ee6470/HW2/Filters_QK/build/CMakeFiles/filters.dir/Initiator.cpp.o"
+  "/home/user/ee6470/HW2/Filters_QK/Testbench.cpp" "/home/user/ee6470/HW2/Filters_QK/build/CMakeFiles/filters.dir/Testbench.cpp.o"
+  "/home/user/ee6470/HW2/Filters_QK/main.cpp" "/home/user/ee6470/HW2/Filters_QK/build/CMakeFiles/filters.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/systemc/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
